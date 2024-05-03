c485cec44aa8b26b77d1e367efa85325a621b208
masud040 <masud040@gmail.com>
2024-05-03 21:08:33 +0600

    add a payment page for handle payment

--- app/components/details/EventDetails.js
+++ app/components/details/EventDetails.js
@@ -5,12 +5,12 @@ export default function EventDetail() {
         <h2 className="text-2xl font-bold">Details</h2>
         <div className="my-2 text-[#AEAEAE] space-y-4 prose lg:prose-lg max-w-none">
           <p className="">
-            If you're passionate about data, coding, and everything
-            tech-related, this is an event you won't want to miss. Whether
-            you're a seasoned developer or just getting started, join us to
-            discuss the latest trends in data analysis, programming, and
-            software development. This is a fantastic opportunity to chat with
-            fellow tech enthusiasts, exchange ideas, and maybe even spark some
+            If you are passionate about data, coding, and everything
+            tech-related, this is an event you won not want to miss. Whether you
+            are a seasoned developer or just getting started, join us to discuss
+            the latest trends in data analysis, programming, and software
+            development. This is a fantastic opportunity to chat with fellow
+            tech enthusiasts, exchange ideas, and maybe even spark some
             brilliant collaborations. Hosted in the 2nd Floor Training Room at
             The Power Plant Business Incubator.
           </p>
